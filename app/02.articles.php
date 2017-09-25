5a753250d0068c0e7e4ad4e33f24147a6504676a
Deff <olegkotsubko@gmail.com>
2017-09-26 00:18:10 +0300

    articles-40%

---
+++ app/02.articles.php
@@ -0,0 +1,111 @@
+<?php include_once "parts/head.php" ?>
+<body class="before-white">
+<?php include_once "parts/header-white.php" ?>
+
+<?php include_once "parts/breadcrumbs.php" ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="page-title">Статьи</h1>
+            <div class="row">
+                <div class="col-xl-3 col-lg-3 col-md-12">
+                    <ul class="left-sidebar">
+                        <li class="left-sidebar__item"><a href="#">Водоснабжение</a></li>
+                        <li class="left-sidebar__item active"><a href="#">Водоотведение</a>
+                            <ul class="left-sidebar-second">
+                                <li class="left-sidebar-second__item"><a href="#">Дренажные насосы</a></li>
+                                <li class="left-sidebar-second__item"><a href="#">Фекальные насосы</a></li>
+                                <li class="left-sidebar-second__item"><a href="#">Комплектующие</a></li>
+                            </ul>
+                        </li>
+                        <li class="left-sidebar__item active"><a href="#">Трубопроводы и арматура</a>
+                        <li class="left-sidebar__item active"><a href="#">Отопление</a>
+                        <li class="left-sidebar__item active"><a href="#">Фильтрация</a>
+                    </ul>
+                </div>
+                <div class="col-xl-9 col-lg-9 col-md-12">
+                    
+                    <ul class="media-list list-unstyled">
+                        <li class="media">
+                            <div class="media-image">
+                                <img src="img/jpeg_files/media-1.jpg" alt="description" width="211" height="211">
+                            </div>
+                            <div class="media-body">
+                                <time class="media-body__time" datetime="27.08.2017">27.08.2017</time>
+                                <p class="media-body__title"><a href="#">Правильный выбор насоса для садового участка:
+                                        мнение профессионалов и опыт дачников</a></p>
+                                <p class="media-body__text">Возможен ли сколь-нибудь простой расчет расхода воды по диаметру
+                                    трубы? Или единственный способ — обращаться к специалистам? Именно этот вопрос мы
+                                    попытаемся сегодня рахзобрать в ходе беседы с опытными садоводами</p>
+                                <p class="media-body__link"><a href="#" class="link-arrow"><span>читать далее</span></a></p>
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-image">
+                                <img src="img/jpeg_files/media-1.jpg" alt="description" width="211" height="211">
+                            </div>
+                            <div class="media-body">
+                                <time class="media-body__time" datetime="27.08.2017">27.08.2017</time>
+                                <p class="media-body__title"><a href="#">Правильный выбор насоса для садового участка:
+                                        мнение профессионалов и опыт дачников</a></p>
+                                <p class="media-body__text">Возможен ли сколь-нибудь простой расчет расхода воды по диаметру
+                                    трубы? Или единственный способ — обращаться к специалистам? Именно этот вопрос мы
+                                    попытаемся сегодня рахзобрать в ходе беседы с опытными садоводами</p>
+                                <p class="media-body__link"><a href="#" class="link-arrow"><span>читать далее</span></a></p>
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-body">
+                                <time class="media-body__time" datetime="27.08.2017">27.08.2017</time>
+                                <p class="media-body__title"><a href="#">Правильный выбор насоса для садового участка:
+                                        мнение профессионалов и опыт дачников</a></p>
+                                <p class="media-body__text">Возможен ли сколь-нибудь простой расчет расхода воды по диаметру
+                                    трубы? Или единственный способ — обращаться к специалистам? Именно этот вопрос мы
+                                    попытаемся сегодня рахзобрать в ходе беседы с опытными садоводами</p>
+                                <p class="media-body__link"><a href="#" class="link-arrow"><span>читать далее</span></a></p>
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-image">
+                                <img src="img/jpeg_files/media-1.jpg" alt="description" width="211" height="211">
+                            </div>
+                            <div class="media-body">
+                                <time class="media-body__time" datetime="27.08.2017">27.08.2017</time>
+                                <p class="media-body__title"><a href="#">Правильный выбор насоса для садового участка:
+                                        мнение профессионалов и опыт дачников</a></p>
+                                <p class="media-body__text">Возможен ли сколь-нибудь простой расчет расхода воды по диаметру
+                                    трубы? Или единственный способ — обращаться к специалистам? Именно этот вопрос мы
+                                    попытаемся сегодня рахзобрать в ходе беседы с опытными садоводами</p>
+                                <p class="media-body__link"><a href="#" class="link-arrow"><span>читать далее</span></a></p>
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-image">
+                                <img src="img/jpeg_files/media-1.jpg" alt="description" width="211" height="211">
+                            </div>
+                            <div class="media-body">
+                                <time class="media-body__time" datetime="27.08.2017">27.08.2017</time>
+                                <p class="media-body__title"><a href="#">Правильный выбор насоса для садового участка:
+                                        мнение профессионалов и опыт дачников</a></p>
+                                <p class="media-body__text">Возможен ли сколь-нибудь простой расчет расхода воды по диаметру
+                                    трубы? Или единственный способ — обращаться к специалистам? Именно этот вопрос мы
+                                    попытаемся сегодня рахзобрать в ходе беседы с опытными садоводами</p>
+                                <p class="media-body__link"><a href="#" class="link-arrow"><span>читать далее</span></a></p>
+                            </div>
+                        </li>
+                    </ul>
+
+                    <?php include_once "parts/pagination.php" ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include_once "parts/footer.php" ?>
+
+    <script src="js/libs.min.js"></script>
+    <script src="js/common.js"></script>
+</body>
+</html>
