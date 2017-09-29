cd685805502d517fcdee2e5d64b7ce5e65436e9d
deff <olegkotsubko@gmail.com>
2017-09-29 18:33:58 +0300

    compare 5%

---
+++ app/11.compare.php
@@ -0,0 +1,81 @@
+<?php include_once "parts/head.php" ?>
+<body class="before-white">
+<?php include_once "parts/header-white.php" ?>
+
+<?php include_once "parts/breadcrumbs.php" ?>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="page-title">Сравнение</h1>
+            <div class="compare">
+                <div class="compare-left-block"></div>
+
+                <div class="compare-right-block mt-minus-top">
+
+                    <ul class="compare-ctrl">
+                        <li class="compare-ctrl__item title"><a href="#">Фекальные насосы <i class="close-sm"></i></a>
+                        </li>
+                        <li class="compare-ctrl__item"><a href="#">Фильтры для воды <i class="close-sm"></i></a></li>
+                    </ul>
+
+                    <div class="compare-slider">
+                        <div>
+                            <div class="compare-slider__item">
+
+                                <a href="#" class="compare-close-button">
+                                    <i class="close-btn"></i>
+                                </a>
+
+                                <div class="goods-compare">
+                                    <div class="goods-image goods-image--gray">
+                                        <a href="#">
+                                            <img src="img/jpeg_files/goods-md-1.png" alt="goods" height="142"
+                                                 width="134">
+                                        </a>
+                                    </div>
+                                    <div class="goods-link">
+                                        <a href="#">BPS 25-6ESA-180</a>
+                                    </div>
+                                    <div class="goods-property">
+                                        <div class="goods-subtitle"><span>Циркуляционный насос</span></div>
+                                        <ul class="goods-property__list">
+                                            <li class="goods-property__list--item">питание: <b>сеть</b></li>
+                                            <li class="goods-property__list--item">мощность: <b>0.6</b></li>
+                                            <li class="goods-property__list--item">производительность: <b>50</b></li>
+                                            <li class="goods-property__list--item">д-р входного отверстия: <b>1”</b>
+                                            </li>
+                                            <li class="goods-property__list--item">д-р выходного отверстия: <b>1”</b>
+                                            </li>
+                                            <li class="goods-property__list--item">страна: <b>Китай</b></li>
+                                        </ul>
+                                        <div class="goods-price">
+                                            <span class="goods-price-current">₴ 12 600 </span>
+                                            <span class="goods-price-old">₴ 13 000</span>
+                                            <span class="goods-add-to-cart"><span
+                                                        class="iconmoon icon-basket"></span></span>
+                                        </div>
+                                        <ul class="goods-property__ctrl">
+                                            <li class="goods-property__ctrl--icon"><a href="#"><span
+                                                            class="iconmoon icon-basket"></span></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!--    --><?php //include_once "parts/contacts.php" ?>
+<?php include_once "parts/footer.php" ?>
+
+<script src="js/libs.min.js"></script>
+<script src="js/common.js"></script>
+</body>
+</html>
